b763318cc5982325c259ad18475c21c03434c0dd
anita-ko <koa0116@gmail.com>
2026-09-20 17:57:22 +0000

    completed lab 1b

--- lab1b.py
+++ lab1b.py
@@ -1,15 +1,26 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Anita Ko
+# Date: Sep 20, 2026
 # Purpose: Use arithmetic in python.
 # Usage: python3 lab1b.py
 
 # TO-DO 1:
 #	Create a variable called "num1", take its value from user.
+num1 = input("Enter the first number: ")
 #	Create another variable called "num2" and take its value from user. 
+num2 = input("Enter the second number: ")
 # Convert the values to integers using int() function
+num1 = int(num1)
+num2 = int(num2)
 
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
+print("num1 + num2 =", num1 + num2)
+print("num1 - num2 =", num1 - num2)
+print("num1 * num2 =", num1 * num2)
+print("num1 ** num2 =", num1 ** num2)
+print("num1 / num2 =", num1 / num2)
+print("num1 // num2 =", num1 // num2)
+print("num1 % num2 =", num1 % num2)
