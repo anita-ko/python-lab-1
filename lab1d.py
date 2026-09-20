f8854d3cb96b0563d08e5947b330977e35f67869
anita-ko <koa0116@gmail.com>
2026-09-20 18:46:20 +0000

    completed lab 1d

--- lab1d.py
+++ lab1d.py
@@ -2,25 +2,40 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Anita Ko
+# Date: Sept 20, 2026
 # Purpose: Use string methods and f-string formating.
 # Usage: python3 lab1d.py
 
 #TO-DO 1:
 #	Create a variable called "name" and assign it the value of your name.
+name = "Anita"
+
 # Use the string method .upper() to convert the name to upper case.
+name = name.upper()
+
 # Create another variable called “age”, the value of “age” should be your age
+age = 24
+
 # The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method. 
+print("How are you {}? Happy {}th birthday!".format(name, age))
 
 #TO-DO 2:
 # Create a variable called "words".
 # The value of words should be "The quick brown fox jumps over the lazy dog".
+words = "The quick brown fox jumps over the lazy dog"
+
 # Use indexing to return the first and 17th charecters of "words" to the user.
+print(words[0])
+print(words[16])
 
 #TO-DO 3:
 # Use negative indexing to return the words "jumps" and "quick" from "words" to the user.
+print(words[-23:-18])
+print(words[-39:-34])
 
 #TO-DO 4:
 # Use slicing to retun everything between index 2-15 to the user.
+print(words[2:15])
 # Print "uick brown foxs ju" from "words".
+print(words[5:22])
